19301827af56ac33290f6d786ce5de7029abd716
KamyabT <dgghost034@gmail.com>
2023-03-07 00:45:38 +0330

    Style Added

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -32,46 +32,84 @@ const Form = () => {
   };
 
   return (
-    <>
-      <main>
-        <section>
-          <div>
-            <form className="formDesign" onSubmit={handleSubmit}>
-              {submitted && validated ? <div>success !!!</div> : null}
-              <input
-                type="text"
-                placeholder="First Name"
-                value={value.firstName}
-                onChange={handleFirstNameChanges}
-              ></input>
-              {submitted && !value.firstName ? (
-                <span>fill the first name</span>
-              ) : null}
-              <input
-                type="text"
-                placeholder="Last Name"
-                value={value.lastName}
-                onChange={handleLastNameChanges}
-              ></input>
-              {submitted && !value.lastName ? (
-                <span>fill the last name</span>
-              ) : null}
-              <input
-                type="email"
-                placeholder="Email"
-                value={value.email}
-                onChange={handleEmailChanges}
-              ></input>
-              {submitted && !value.email ? (
-                <span>fill the email</span>
-              ) : null}
-              <input type="submit" value="Submit"></input>
-            </form>
-          </div>
-        </section>
-      </main>
-    </>
+    <main className="mainDesign">
+      <section className="sectionDesign">
+        <div className="formContainerDesign">
+          <p className="headerDesign">Registeration Form</p>
+          <form className="formDesign" onSubmit={handleSubmit}>
+            {submitted && validated ? <div className="successAlert">success !!!</div> : null}
+            <input
+              type="text"
+              placeholder="First Name"
+              value={value.firstName}
+              onChange={handleFirstNameChanges}
+              className="inputDesign"
+            ></input>
+            {submitted && !value.firstName ? (
+              <span className="alertDesign">fill the first name</span>
+            ) : null}
+            <input
+              type="text"
+              placeholder="Last Name"
+              value={value.lastName}
+              onChange={handleLastNameChanges}
+              className="inputDesign"
+            ></input>
+            {submitted && !value.lastName ? (
+              <span className="alertDesign">fill the last name</span>
+            ) : null}
+            <input
+              type="email"
+              placeholder="Email"
+              value={value.email}
+              onChange={handleEmailChanges}
+              className="inputDesign"
+            ></input>
+            {submitted && !value.email ? <span className="alertDesign">fill the email</span> : null}
+            <input
+              type="submit"
+              value="Submit"
+              className="inputDesign submitDesign"
+            ></input>
+          </form>
+        </div>
+      </section>
+    </main>
   );
 };
 
 export default Form;
+
+{
+  /* <form className="formDesign" onSubmit={handleSubmit}>
+{submitted && validated ? <div>success !!!</div> : null}
+<input
+  type="text"
+  placeholder="First Name"
+  value={value.firstName}
+  onChange={handleFirstNameChanges}
+></input>
+{submitted && !value.firstName ? (
+  <span>fill the first name</span>
+) : null}
+<input
+  type="text"
+  placeholder="Last Name"
+  value={value.lastName}
+  onChange={handleLastNameChanges}
+></input>
+{submitted && !value.lastName ? (
+  <span>fill the last name</span>
+) : null}
+<input
+  type="email"
+  placeholder="Email"
+  value={value.email}
+  onChange={handleEmailChanges}
+></input>
+{submitted && !value.email ? (
+  <span>fill the email</span>
+) : null}
+<input type="submit" value="Submit"></input>
+</form> */
+}
